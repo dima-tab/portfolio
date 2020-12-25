8edc0a6acb7c82a3752b41bebcc7b9dec1381bf8
dima-tab <onlygsmart@gmail.com>
2020-12-25 20:30:42 +0200

    added express server

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+// const cors = require('cors')
+const app = express()
+
+// app.options('*', cors())
+//
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*')
+//
+//   // authorized headers for preflight requests
+//   // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+//   next()
+//
+//   app.options('*', (req, res) => {
+//     // allowed XHR methods
+//     res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS')
+//     res.send()
+//   })
+// })
+
+app.get('/', function (req, res) {
+  console.log("app.get('/')")
+  res.send('Hello, Misa!')
+})
+
+app.listen(3000)
+
+// for start server, open terminal and write
+// npm run server
+// can check it on http://localhost:3000/
