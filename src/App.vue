8d3a1d521c21fc5967730b70348bdcf2bde4c49a
dima-tab <onlygsmart@gmail.com>
2020-12-25 17:34:38 +0200

    test 24_12_20

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="ipsta!!!"/>
+<!--  <img alt="Vue logo" src="./assets/logo.png">-->
+  <HelloWorld msg="MSG"/>
 </template>
 
 <script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,80 @@
 <template>
-  <div class="hello">
+  <div class='hello'>
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+    <p>{{ count }}</p>
+    <p>{{ fnc(count) }}</p>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li
+        v-for="(item, index) in arr"
+        :key="'key_' + item.value"
+      >
+        - index: {{ index }} |
+        - value: {{ item.value }} |
+        - label: {{ item.label }}
+      </li>
     </ul>
-    <h3>Essential Links</h3>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+      <li
+        v-for="(item, key) in bitcoinInfo"
+        :key="'key_' + item.value"
+      >
+        - key: {{ key }} |
+        - code: {{ item.code }} |
+        - rate: {{ item.rate_float }}
+      </li>
     </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <button @click='count++'>Кликни меня полностью для +1</button>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data: function () {
+    return {
+      bitcoinInfo: null,
+      count: 0,
+      arr: [
+        { label: 'one', value: 1 },
+        { label: 'two', value: 2 },
+        { label: 'three', value: 3 }
+      ]
+    }
+  },
+  methods: {
+    fnc: num => num * num * num
+  },
+  mounted: function () { // init
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => {
+        this.bitcoinInfo = response.data.bpi
+        console.log(response.data)
+      })
+      .catch(err => console.error(err))
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
 }
 ul {
+  text-align: left;
   list-style-type: none;
   padding: 0;
+  max-width: 400px;
+  margin: 0 auto;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+  margin: 10px;
 }
 a {
   color: #42b983;
