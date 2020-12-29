f69b50f2c2ac01dbea4348b8a13c9085d9235d18
pouri <pouri@gmail.com>
2020-12-29 19:31:32 +0200

    Add color schemes to project

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,34 +1,79 @@
 <template>
-  <div class='hello'>
-    <h1>{{ msg }}</h1>
-    <p>{{ count }}</p>
-    <p>{{ fnc(count) }}</p>
-    <ul>
-      <li
-        v-for="(item, index) in arr"
-        :key="'key_' + item.value"
-      >
-        - index: {{ index }} |
-        - value: {{ item.value }} |
-        - label: {{ item.label }}
-      </li>
-    </ul>
-    <ul>
-      <li
-        v-for="(item, key) in bitcoinInfo"
-        :key="'key_' + item.value"
-      >
-        - key: {{ key }} |
-        - code: {{ item.code }} |
-        - rate: {{ item.rate_float }}
-      </li>
-    </ul>
-    <button @click='count++'>Кликни меня полностью для +1</button>
-  </div>
+<section class="root">
+    <div class="header-secton">
+        <div class="center">
+            <p>Ololo <span class="highlight">ololo</span></p>
+        </div>
+    </div>
+    <div class="main-section">
+        <div class="center">
+          <div class='hello'>
+            <h1>{{ msg }}</h1>
+            <p>{{ count }}</p>
+            <p>{{ fnc(count) }}</p>
+            <ul>
+              <li
+                v-for="(item, index) in arr"
+                :key="'key_' + item.value"
+              >
+                - index: {{ index }} |
+                - value: {{ item.value }} |
+                - label: {{ item.label }}
+              </li>
+            </ul>
+            <ul>
+              <li
+                v-for="(item, key) in bitcoinInfo"
+                :key="'key_' + item.value"
+              >
+                - key: {{ key }} |
+                - code: {{ item.code }} |
+                - rate: {{ item.rate_float }}
+              </li>
+            </ul>
+            <button @click='count++'>Кликни меня полностью для +1</button>
+          </div>
+        </div>
+    </div>
+    <div class="footer-section">
+        <div class="center">
+            <div class="colorScheme-block">
+                <div class="colorScheme-btn" @click="colorSchemeToggle">
+                  <span class="colorScheme-dot" v-bind:style="{transform: `translateY(${colorSchemeCounter * 100}%)`}"></span>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 </template>
 
 <script>
 import axios from 'axios'
+
+// __________ ColorScheme toggle __________
+// const colorSchemeBtn = document.getElementsByClassName('colorScheme-btn')
+const colorSchemeArray = [
+  { // custom
+    '--Primary': '#0c101c',
+    '--Secondary': '#394b65',
+    '--FontColor': '#fff',
+    '--HighLight': '#7ba0d9'
+  },
+  { // dark
+    '--Primary': '#000',
+    '--Secondary': '#1b1b1b',
+    '--FontColor': '#fff',
+    '--HighLight': '#f90'
+  },
+  { // light
+    '--Primary': '#b7b7b7',
+    '--Secondary': '#e1e1e1',
+    '--FontColor': '#7a7a7a',
+    '--HighLight': '#040404'
+  }
+]
+const colorSchemeLength = colorSchemeArray.length
+// __________ //ColorScheme toggle __________
 
 export default {
   name: 'HelloWorld',
@@ -39,6 +84,7 @@ export default {
     return {
       bitcoinInfo: null,
       count: 0,
+      colorSchemeCounter: 0,
       arr: [
         { label: 'one', value: 1 },
         { label: 'two', value: 2 },
@@ -47,7 +93,18 @@ export default {
     }
   },
   methods: {
-    fnc: num => num * num * num
+    fnc: num => num * num * num,
+    colorSchemeToggle: function () {
+      if (this.colorSchemeCounter < colorSchemeLength - 1) {
+        this.colorSchemeCounter++
+      } else {
+        this.colorSchemeCounter = 0
+      }
+      const Obj = colorSchemeArray[this.colorSchemeCounter]
+      for (const key in Obj) {
+        document.documentElement.style.setProperty(key, Obj[key])
+      }
+    }
   },
   mounted: function () { // init
     axios
@@ -59,6 +116,7 @@ export default {
       .catch(err => console.error(err))
   }
 }
+
 </script>
 
 <!-- Add 'scoped' attribute to limit CSS to this component only -->
@@ -78,5 +136,27 @@ li {
 }
 a {
   color: #42b983;
+}
+.colorScheme-btn {
+  height: 22px;
+  width: 10px;
+  position: relative;
+  border: 1px solid var(--Secondary);
+  border-radius: 5px;
+  cursor: pointer;
+}
+.colorScheme-dot {
+  content: "";
+  position: absolute;
+  height: 6px;
+  width: 6px;
+  top: 1px;
+  right: 0;
+  left: 0;
+  margin: 0 auto;
+  background-color: var(--Secondary);
+  border-radius: 50%;
+  transform: translateY(0);
+  transition: transform .3s linear;
 }
 </style>
