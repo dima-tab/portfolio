b1d3e1e36094404f051572534002a9e9d00f0113
Ksunja <ksenja.jg@gmail.com>
2021-11-22 18:18:30 +0200

    slide description, optimization css

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,75 +1,75 @@
 <template>
-	<section class="root">
-		<div class="header-section">
-			<div>
-				FONT AWESOME
-				<i class="far fa-question-circle" /> <!-- solid style of the question circle icon -->
-				<i class="fas fa-question-circle" /> <!-- solid style of the question circle icon -->
-				<i class="fab fa-facebook" />        <!-- facebook brand icon-->
-				<i class="fab fa-facebook-f" />      <!-- facebook "f" brand icon-->
-			</div>
-			<div class="center">
-				<p>Portfolio <span class="highlight">project</span></p>
-			</div>
-		</div>
-		<div class="main-section">
-			<SliderDrum />
-			<div class="center">
-				<div id="nav">
-					<router-link to="/">
-						Home
-					</router-link>
-					|
-					<router-link to="/about">
-						About
-					</router-link>
-					|
-					<router-link to="/profile">
-						Profile
-					</router-link>
-				</div>
-				<router-view />
-				<!--          <div class='hello'>-->
-				<!--            <h1>{{ msg }}</h1>-->
-				<!--            <p>{{ count }}</p>-->
-				<!--            <p>{{ fnc(count) }}</p>-->
-				<!--            <ul>-->
-				<!--              <li-->
-				<!--                v-for="(item, index) in arr"-->
-				<!--                :key="'key_' + item.value"-->
-				<!--              >-->
-				<!--                - index: {{ index }} |-->
-				<!--                - value: {{ item.value }} |-->
-				<!--                - label: {{ item.label }}-->
-				<!--              </li>-->
-				<!--            </ul>-->
-				<!--            <ul>-->
-				<!--              <li-->
-				<!--                v-for="(item, key) in bitcoinInfo"-->
-				<!--                :key="'key_' + item.value"-->
-				<!--              >-->
-				<!--                - key: {{ key }} |-->
-				<!--                - code: {{ item.code }} |-->
-				<!--                - rate: {{ item.rate_float }}-->
-				<!--              </li>-->
-				<!--            </ul>-->
-				<!--            <button @click='count++'>Кликни меня полностью для +1</button>-->
-				<!--          </div>-->
-			</div>
-		</div>
-		<div class="footer-section">
-			<div class="center">
-				<div class="colorScheme-block">
-					<div class="colorScheme-btn" @click="colorSchemeToggle">
+   <section class="root">
+      <div class="header-section">
+         <div>
+            FONT AWESOME
+            <i class="far fa-question-circle"/> <!-- solid style of the question circle icon -->
+            <i class="fas fa-question-circle"/> <!-- solid style of the question circle icon -->
+            <i class="fab fa-facebook"/>        <!-- facebook brand icon-->
+            <i class="fab fa-facebook-f"/>      <!-- facebook "f" brand icon-->
+         </div>
+         <div class="center">
+            <p>Portfolio <span class="highlight">project</span></p>
+         </div>
+      </div>
+      <div class="main-section">
+         <SliderDrum/>
+         <div class="center">
+            <div id="nav">
+               <router-link to="/">
+                  Home
+               </router-link>
+               |
+               <router-link to="/about">
+                  About
+               </router-link>
+               |
+               <router-link to="/profile">
+                  Profile
+               </router-link>
+            </div>
+            <router-view/>
+            <!--          <div class='hello'>-->
+            <!--            <h1>{{ msg }}</h1>-->
+            <!--            <p>{{ count }}</p>-->
+            <!--            <p>{{ fnc(count) }}</p>-->
+            <!--            <ul>-->
+            <!--              <li-->
+            <!--                v-for="(item, index) in arr"-->
+            <!--                :key="'key_' + item.value"-->
+            <!--              >-->
+            <!--                - index: {{ index }} |-->
+            <!--                - value: {{ item.value }} |-->
+            <!--                - label: {{ item.label }}-->
+            <!--              </li>-->
+            <!--            </ul>-->
+            <!--            <ul>-->
+            <!--              <li-->
+            <!--                v-for="(item, key) in bitcoinInfo"-->
+            <!--                :key="'key_' + item.value"-->
+            <!--              >-->
+            <!--                - key: {{ key }} |-->
+            <!--                - code: {{ item.code }} |-->
+            <!--                - rate: {{ item.rate_float }}-->
+            <!--              </li>-->
+            <!--            </ul>-->
+            <!--            <button @click='count++'>Кликни меня полностью для +1</button>-->
+            <!--          </div>-->
+         </div>
+      </div>
+      <div class="footer-section">
+         <div class="center">
+            <div class="colorScheme-block">
+               <div class="colorScheme-btn" @click="colorSchemeToggle">
 						<span
-							class="colorScheme-dot"
-							:style="{transform: `translateY(${colorSchemeCounter * 100}%)`}"
-						/>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
+                     class="colorScheme-dot"
+                     :style="{transform: `translateY(${colorSchemeCounter * 100}%)`}"
+                  />
+               </div>
+            </div>
+         </div>
+      </div>
+   </section>
 </template>
 
 <script>
@@ -82,82 +82,81 @@ import 'toastr/build/toastr.css'
 import SliderDrum from './SliderDrum.vue'
 
 const colorSchemeArray = [
-	{ // custom
-		'--Primary': '#0c101c',
-		'--Secondary': '#394b65',
-		'--FontColor': '#fff',
-		'--HighLight': '#7ba0d9'
-	},
-	{ // dark
-		'--Primary': '#000',
-		'--Secondary': '#1b1b1b',
-		'--FontColor': '#fff',
-		'--HighLight': '#f90'
-	},
-	{ // light
-		'--Primary': '#b7b7b7',
-		'--Secondary': '#e1e1e1',
-		'--FontColor': '#7a7a7a',
-		'--HighLight': '#040404'
-	}
+  { // custom
+    '--Primary': '#0c101c',
+    '--Secondary': '#394b65',
+    '--FontColor': '#fff',
+    '--HighLight': '#7ba0d9'
+  },
+  { // dark
+    '--Primary': '#000',
+    '--Secondary': '#1b1b1b',
+    '--FontColor': '#fff',
+    '--HighLight': '#f90'
+  },
+  { // light
+    '--Primary': '#b7b7b7',
+    '--Secondary': '#e1e1e1',
+    '--FontColor': '#7a7a7a',
+    '--HighLight': '#040404'
+  }
 ]
 
 export default {
-	name: 'MainComponent',
-	components: { SliderDrum },
-	props: {
-		msg: String
-	},
-	data: function() {
-		return {
-			bitcoinInfo: null,
-			count: 0,
-			colorSchemeCounter: 0,
-			arr: [
-				{
-					label: 'one',
-					value: 1
-				},
-				{
-					label: 'two',
-					value: 2
-				},
-				{
-					label: 'three',
-					value: 3
-				}
-			]
-		}
-	},
-	methods: {
-		fnc: num => num * num * num,
-		colorSchemeToggle: function() {
-			// toastr.warning('My name is Inigo Montoya. You killed my father, prepare to die!', 'title', { timeOut: 5000 })
-			// toastr.success('success !', 'success title', { timeOut: 5000 })
-			// toastr.error('error !', 'error title', { timeOut: 5000 })
-			// toastr.info('some info!', 'info title', { timeOut: 5000 })
-			if (this.colorSchemeCounter < colorSchemeArray.length - 1) {
-				this.colorSchemeCounter++
-			}
-			else {
-				this.colorSchemeCounter = 0
-			}
-			const Obj = colorSchemeArray[this.colorSchemeCounter]
-			for (const key in Obj) {
-				document.documentElement.style.setProperty(key, Obj[key])
-			}
-			toastr.info('success!', 'The color scheme has been changed', { timeOut: 3000 })
-		}
-	},
-	mounted: function() { // init
-		axios
-			.get('https://api.coindesk.com/v1/bpi/currentprice.json')
-			.then(response => {
-				this.bitcoinInfo = response.data.bpi
-				console.log(response.data)
-			})
-			.catch(err => console.error(err))
-	}
+  name: 'MainComponent',
+  components: { SliderDrum },
+  props: {
+    msg: String
+  },
+  data: function () {
+    return {
+      bitcoinInfo: null,
+      count: 0,
+      colorSchemeCounter: 0,
+      arr: [
+        {
+          label: 'one',
+          value: 1
+        },
+        {
+          label: 'two',
+          value: 2
+        },
+        {
+          label: 'three',
+          value: 3
+        }
+      ]
+    }
+  },
+  methods: {
+    fnc: num => num * num * num,
+    colorSchemeToggle: function () {
+      // toastr.warning('My name is Inigo Montoya. You killed my father, prepare to die!', 'title', { timeOut: 5000 })
+      // toastr.success('success !', 'success title', { timeOut: 5000 })
+      // toastr.error('error !', 'error title', { timeOut: 5000 })
+      // toastr.info('some info!', 'info title', { timeOut: 5000 })
+      if (this.colorSchemeCounter < colorSchemeArray.length - 1) {
+        this.colorSchemeCounter++
+      } else {
+        this.colorSchemeCounter = 0
+      }
+      const Obj = colorSchemeArray[this.colorSchemeCounter]
+      for (const key in Obj) {
+        document.documentElement.style.setProperty(key, Obj[key])
+      }
+      toastr.info('success!', 'The color scheme has been changed', { timeOut: 3000 })
+    }
+  },
+  mounted: function () { // init
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => {
+        this.bitcoinInfo = response.data.bpi
+        console.log(response.data)
+      })
+      .catch(err => console.error(err))
+  }
 }
 
 </script>
@@ -165,46 +164,46 @@ export default {
 <!-- Add 'scoped' attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
-	margin: 40px 0 0;
+   margin: 40px 0 0;
 }
 
 ul {
-	text-align: left;
-	list-style-type: none;
-	padding: 0;
-	max-width: 400px;
-	margin: 0 auto;
+   text-align: left;
+   list-style-type: none;
+   padding: 0;
+   max-width: 400px;
+   margin: 0 auto;
 }
 
 li {
-	margin: 10px;
+   margin: 10px;
 }
 
 a {
-	color: #42b983;
+   color: #42b983;
 }
 
 .colorScheme-btn {
-	height: 32px;
-	width: 14px;
-	position: relative;
-	border: 2px solid var(--Secondary);
-	border-radius: 7px;
-	cursor: pointer;
+   height: 32px;
+   width: 14px;
+   position: relative;
+   border: 2px solid var(--Secondary);
+   border-radius: 7px;
+   cursor: pointer;
 }
 
 .colorScheme-dot {
-	content: "";
-	position: absolute;
-	height: 8px;
-	width: 8px;
-	top: 2px;
-	left: 0;
-	right: 0;
-	margin: 0 auto;
-	background-color: var(--HighLight);
-	border-radius: 50%;
-	transform: translateY(0);
-	transition: transform .3s linear;
+   content: "";
+   position: absolute;
+   height: 8px;
+   width: 8px;
+   top: 2px;
+   left: 0;
+   right: 0;
+   margin: 0 auto;
+   background-color: var(--HighLight);
+   border-radius: 50%;
+   transform: translateY(0);
+   transition: transform .3s linear;
 }
 </style>
--- src/components/SliderDrum.vue
+++ src/components/SliderDrum.vue
@@ -1,325 +1,296 @@
 <template>
-	<div
-		class="wrapper"
-		tabindex="-1"
-		@keyup.left="offsetBy(-1)"
-		@keyup.right="offsetBy(1)"
-		@scroll="offsetBy(-1)"
-	>
-		<div class="slider">
-			<a href="#" class="btn btn-left" @click="offsetBy(-1)">
-				<i class="fas fa-chevron-left" />
-			</a>
-			<div v-if="!!images?.length" class="slider-list">
-				<div
-					v-for="img in slides"
-					:key="img.url"
-					class="slide"
-					@click="offsetBy(5 - img.position)"
-				>
-					<a href="#">
-						<img :src="img.url" alt="pony">
-					</a>
-					<p class="info">
-						Lorem ipsum dolor sit amet.
-					</p>
-					<button class="details">
-						details
-					</button>
-				</div>
-			</div>
-			<a href="#" class="btn btn-right" @click="offsetBy(1)">
-				<i class="fas fa-chevron-right" />
-			</a>
-		</div>
-	</div>
+   <div
+      class="wrapper"
+      tabindex="-1"
+      @keyup.left="offsetBy(-1)"
+      @keyup.right="offsetBy(1)"
+   >
+      <div class="slider">
+         <a href="#" class="btn btn-left" @click="offsetBy(-1)">
+            <i class="fas fa-chevron-left"/>
+         </a>
+         <div v-if="!!images?.length" class="slider-list">
+            <div
+               v-for="img in slides"
+               :key="img.url"
+               class="slide"
+               @click="offsetBy(5 - img.position)"
+            >
+               <a href="#">
+                  <img :src="img.url" alt="pony">
+               </a>
+               <p class="info">
+                  Lorem ipsum dolor sit amet.
+               </p>
+               <button class="details"
+               @click="showDetails = !showDetails">
+                  details
+               </button>
+            </div>
+         </div>
+         <a href="#" class="btn btn-right" @click="offsetBy(1)">
+            <i class="fas fa-chevron-right"/>
+         </a>
+      </div>
+   </div>
+   <div v-if="showDetails"
+      class="description">
+      {{ currentDescription }}
+   </div>
 </template>
 
 <script>
 import toastr from 'toastr'
 import 'toastr/build/toastr.css'
+
 const imgArr = require.context('../assets/slides', false, /\.(png|jpe?g|svg)$/)
 
 export default {
-	name: 'SliderDrum',
-	data() {
-		return {
-			order: 1,
-			slides: null,
-		}
-	},
-	methods: {
-		offsetBy: function(num) {
-			if (!this?.images?.length) return toastr.error('Images not loaded!', 'Error', { timeOut: 3500 });
+  name: 'SliderDrum',
+  data () {
+    return {
+      order: 1,
+      slides: null,
+      showDetails: false
+    }
+  },
+  methods: {
+    offsetBy: function (num) {
+      if (!this?.images?.length) return toastr.error('Images not loaded!', 'Error', { timeOut: 3500 })
 
-			while (num > 0) { // to right
-				this.slides.unshift(...this.slides.splice(this.slides.length - 1, 1));
-				num--;
-			}
-			while (num < 0) { // to left
-				this.slides.push(this.slides.shift());
-				num++;
-			}
-			this.slides.forEach((slide, i) => {
-				slide.position = i + 1;
-			})
-		}
-	},
-	computed: {
-		images: () => imgArr.keys().map((img, i) => {
-			return { url: imgArr(img), position: i + 1 }
-		})
-	},
-	mounted: function() {
-		this.slides = this.images;
-	},
-	created: function() {
-		window.addEventListener('wheel', e => {
-			this.offsetBy(Math.round(e.deltaY / 120));
-			// console.log(e);
-		})
-	},
+      while (num > 0) { // to right
+        this.slides.unshift(...this.slides.splice(this.slides.length - 1, 1))
+        num--
+      }
+      while (num < 0) { // to left
+        this.slides.push(this.slides.shift())
+        num++
+      }
+      this.slides.forEach((slide, i) => {
+        slide.position = i + 1
+      })
+    }
+  },
+  computed: {
+    images: () => imgArr.keys().map((img, i) => {
+      return { url: imgArr(img), position: i + 1 }
+    }),
+    currentDescription: function () { return this.slides ? this.slides[5]?.url : null }
+  },
+  mounted: function () {
+    this.slides = this.images
+  },
+  created: function () {
+    window.addEventListener('wheel', e => {
+      this.offsetBy(Math.round(e.deltaY / 120))
+      // console.log(e);
+    })
+  }
 }
 </script>
 
 <style scoped>
 .wrapper {
-	display: flex;
-	justify-content: center;
-	padding: 0 5px;
-	position: relative;
-	outline: none;
+   position: relative;
+   display: flex;
+   justify-content: center;
+   padding: 0 5px;
+   outline: none;
 }
 
 .slider {
-	height: 225px;
-	padding: 15px 0;
-	/*background-color: darkcyan;*/
-	scroll-snap-align: center;
+   height: 225px;
+   padding: 15px 0;
 }
 
 /* region Buttons */
 .btn {
-	font-size: 24px;
-	position: absolute;
-	top: calc(50% - 9px);
-	color: var(--HighLight);
+   position: absolute;
+   top: calc(50% - 9px);
+   font-size: 24px;
+   color: var(--HighLight);
 }
 
 .btn-left {
-	left: 10px;
+   left: 10px;
 }
 
 .btn-right {
-	right: 10px;
+   right: 10px;
 }
 
+.details {
+   position: absolute;
+   right: 5px;
+   bottom: 15px;
+   padding: 5px;
+   border: darkgrey 2px solid;
+   border-radius: 15px;
+   background: var(--HighLight);
+   color: var(--Primary);
+   transition: 0.3s;
+}
+.details:hover {
+   padding: 6px;
+   border: darkgrey 1px solid;
+   background: var(--Secondary);
+   color: var(--HighLight);
+   transition: 0.3s;
+}
 /* endregion */
 
 /* region Slider */
 .slider-list {
-	position: relative;
-	display: flex;
-	align-items: center;
-	max-width: 1200px;
-	min-height: 100%;
-	overflow: hidden;
-	/*overflow-x: scroll;*/
-	/*scroll-snap-type: x mandatory;*/
+   position: relative;
+   display: flex;
+   align-items: center;
+   max-width: 1200px;
+   min-height: 100%;
+   overflow: hidden;
 }
 
 .slide {
-	margin-right: 15px;
-	transition: all 1s ease;
-	border-radius: 15px;
-	border: darkgrey 2px solid;
-	box-sizing: border-box;
-	background: white;
-	position: relative;
+   position: relative;
+   margin-right: 15px;
+   border-radius: 15px;
+   border: darkgrey 2px solid;
+   box-sizing: border-box;
+   background: white;
 }
 
 .slider-list img {
-	width: 60px;
-	height: 50px;
-	border-radius: 15px 15px 0 0;
-	object-fit: cover;
+   width: 60px;
+   height: 50px;
+   border-radius: 15px 15px 0 0;
+   object-fit: cover;
 }
 
 .info {
-	color: #0c101c;
-	width: 50px;
-	overflow: hidden;
-	white-space: nowrap;
-	text-overflow: ellipsis;
-	padding: 5px;
+   width: 50px;
+   padding: 5px;
+   overflow: hidden;
+   white-space: nowrap;
+   text-overflow: ellipsis;
+   font-size: 12px;
+   color: #0c101c;
 }
 
-.details {
-	background: #7ba0d9;
-	border: darkgrey 2px solid;
-	padding: 5px;
-	border-radius: 15px;
-	position: absolute;
-	right: 5px;
-	bottom: 15px;
+.description{
+   width: 70%;
+   margin: 40px auto;
+   text-align: center;
 }
 
-.slider-list > div:not(:nth-child(5)) {
-	filter: blur(1px);
-	opacity: 0.7;
+.slider-list > div:not(:nth-child(5)) img{
+   opacity: 0.7;
 }
 
-.slider-list > div:not(:nth-child(5)):hover {
-	filter: none;
-	opacity: 1;
-	box-sizing: border-box;
+.slider-list > div:not(:nth-child(5)):hover img {
+   filter: none;
+   opacity: 1;
+   box-sizing: border-box;
 }
 
 .slider-list > div:nth-child(1),
 .slider-list > div:nth-child(9) {
-	opacity: 0;
+   opacity: 0;
 }
 
 .slider-list > div:nth-child(1) {
-	margin-left: -75px;
+   margin-left: -77px;
 }
 
 .slider-list > div:nth-child(2) img,
 .slider-list > div:nth-child(8) img {
-	width: 110px;
-	height: 90px;
+   width: 110px;
+   height: 90px;
+}
+.slider-list > div:nth-child(2) button,
+.slider-list > div:nth-child(8) button {
+   display: none;
+}
+.slider-list > div:nth-child(2) p,
+.slider-list > div:nth-child(8) p {
+   width: 100px;
 }
 
 .slider-list > div:nth-child(3) img,
 .slider-list > div:nth-child(7) img {
-	width: 140px;
-	height: 120px;
+   width: 140px;
+   height: 120px;
 }
 
 .slider-list > div:nth-child(3) p,
 .slider-list > div:nth-child(7) p {
-	width: 100px;
+   width: 100px;
 }
 
 .slider-list > div:nth-child(5) img {
-	width: 220px;
-	height: 200px;
-	opacity: 1;
+   width: 220px;
+   height: 200px;
+   opacity: 1;
 }
 
 .slider-list > div:nth-child(5) p {
-	width: 160px;
+   width: 160px;
 }
 
 .slider-list > div:nth-child(4) img,
 .slider-list > div:nth-child(6) img {
-	width: 180px;
-	height: 150px;
+   width: 180px;
+   height: 150px;
 }
 
 .slider-list > div:nth-child(4) p,
 .slider-list > div:nth-child(6) p {
-	width: 130px;
+   width: 130px;
 }
 
 .slider-list * {
-	transition: all 1.2s ease;
+   transition: all 1.2s ease;
 }
 
 /* endregion */
 
 /* region @media */
 @media (max-width: 1200px) {
-	.slider-list {
-		max-width: 970px;
-		height: 210px;
-		overflow: hidden;
-	}
+   .slider-list {
+      max-width: 940px;
+   }
 
-	.slider-list > div:nth-child(1),
-	.slider-list > div:nth-child(2) {
-		position: absolute;
-		left: -180px;
-	}
-
-	.slider-list > div:nth-child(3) img,
-	.slider-list > div:nth-child(7) img {
-		width: 160px;
-		height: 150px;
-		object-fit: cover;
-	}
-
-	.slider-list > div:nth-child(4) img,
-	.slider-list > div:nth-child(6) img {
-		width: 180px;
-		height: 180px;
-		object-fit: cover;
-	}
-
-	.slider-list > div:nth-child(5) img {
-		width: 230px;
-		height: 210px;
-		object-fit: cover;
-	}
-
-	.slider-list > div:nth-child(7) {
-		margin: 0px;
-	}
+   .slider-list > div:nth-child(1),
+   .slider-list > div:nth-child(2) {
+      position: absolute;
+      left: -180px;
+   }
 }
 
 @media (max-width: 992px) {
-	.slider-list {
-		max-width: 690px;
-		height: 210px;
-		overflow: hidden;
-	}
+   .slider-list {
+      max-width: 622px;
+   }
 
-	.slider-list > div:nth-child(1), .slider-list > div:nth-child(2), .slider-list > div:nth-child(3) {
-		position: absolute;
-		left: -210px;
-	}
-
-	.slider-list > div:nth-child(4) img, .slider-list > div:nth-child(6) img {
-		width: 210px;
-		height: 180px;
-		object-fit: cover;
-	}
-
-	.slider-list > div:nth-child(5) img {
-		width: 240px;
-		height: 210px;
-		object-fit: cover;
-	}
-
-	.slider-list > div:nth-child(6) {
-		margin: 0px;
-	}
+   .slider-list > div:nth-child(1), .slider-list > div:nth-child(2), .slider-list > div:nth-child(3) {
+      position: absolute;
+      left: -210px;
+   }
 }
 
 @media (max-width: 767px) {
-	.slider-list {
-		max-width: 280px;
-		height: 210px;
-		overflow: hidden;
-		flex-direction: column;
-	}
+   .slider-list {
+      max-width: 230px;
+   }
 
-	.slider-list > div:nth-child(4), .slider-list > div:nth-child(1), .slider-list > div:nth-child(2), .slider-list > div:nth-child(3) {
-		position: absolute;
-		left: -240px;
-	}
+   .slider-list > div:nth-child(4), .slider-list > div:nth-child(1), .slider-list > div:nth-child(2),
+   .slider-list > div:nth-child(3) {
+      position: absolute;
+      left: -245px;
+   }
 
-	.slider-list > div:nth-child(5) img {
-		width: 240px;
-		height: 210px;
-		object-fit: cover;
-	}
-
-	.slider-list > div:nth-child(4) img, .slider-list > div:nth-child(6) img {
-		width: 240px;
-		height: 210px;
-		object-fit: cover;
-	}
+   .slider-list > div:nth-child(4) img, .slider-list > div:nth-child(6) img {
+      width: 220px;
+      height: 200px;
+   }
 }
 
 /* endregion */
